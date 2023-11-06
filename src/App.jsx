ccbcf5d554d0c27d931840c59dd7ddae4d49b630
Michael ogozi <michealogozi@gmail.com>
2023-11-06 21:20:01 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,9 @@
+function home() {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  );
+}
+
+export default home;
